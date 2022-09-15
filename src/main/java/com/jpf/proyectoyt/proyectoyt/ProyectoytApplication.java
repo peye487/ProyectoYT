c0e25b779c40065e40f02e7cee15694b9a92ef6c
Juan Pablo Ferreyra <jpferreyra84@yahoo.com>
2022-09-15 10:51:26 -0300

    Commit 1. project started.

---
+++ src/main/java/com/jpf/proyectoyt/proyectoyt/ProyectoytApplication.java
@@ -0,0 +1,13 @@
+package com.jpf.proyectoyt.proyectoyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoytApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoytApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpf/proyectoyt/proyectoyt/ProyectoytApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpf.proyectoyt.proyectoyt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoytApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
